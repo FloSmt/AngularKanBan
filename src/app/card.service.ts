a8f797f405a3fd77dbb170d39f70c8751a774433
Bxnnx02 <119310014+Bxnnx02@users.noreply.github.com>
2023-07-28 13:51:50 +0200

    changed card input for push parameter

--- src/app/card.service.ts
+++ src/app/card.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import {Card} from "./card";
+import { Card } from "./card";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
+  private columns : Card[][] = [[], [], [],[]];
 
-  cards:Card[] = [];
 
-  constructor() { }
+  addCardsToColumn(columnIndex: number, title:string, content:string) {
+    this.columns[columnIndex - 1].push({id:1, title, content});
+  }
 
-  addCard(card:Card) {
-    this.cards.push(card);
+
+  getColumns() : Card[][] {
+    return this.columns;
   }
 }
+
