7ce9bba566f4e81d7db07dde21166068b5d4f21e
Bxnnx02 <119310014+Bxnnx02@users.noreply.github.com>
2023-07-31 10:39:02 +0200

    card indexing is now global

--- src/app/card.service.ts
+++ src/app/card.service.ts
@@ -6,25 +6,17 @@ import { Card } from "./card";
 })
 export class CardService {
   private columns : Card[][] = [[], [], [],[]];
+  private idCounter: number = 1;
 
   addCardsToColumn(columnIndex: number, title:string, content:string) {
     if(columnIndex >= 1 && columnIndex <= 4){
-      const newCard: Card = {id: this.getNextId(columnIndex), title:title, content:content};
+      const newCard: Card = {id: this.getNextId(), title:title, content:content};
       this.columns[columnIndex-1].push(newCard);
     }
   }
 
-  private getNextId(columnIndex:number): number {
-    let maxId = 0;
-    const column = this.columns[columnIndex -1];
-    this.columns.forEach((card) => {
-      column.forEach((card) => {
-        if (card.id > maxId) {
-          maxId = card.id;
-        }
-      });
-    });
-    return maxId + 1;
+  private getNextId(): number {
+    return this.idCounter++;
   }
 
 
