28373714b5144421edbf10c3803102eb71bce0c6
FloSmt <109459476+FloSmt@users.noreply.github.com>
2023-08-01 12:55:13 +0200

    card delete function

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 import {Priority} from "../priority";
 import {Card} from "../card";
