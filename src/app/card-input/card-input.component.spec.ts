d86dc253894c3ffb7ff2b5deaf72877137d978f5
FloSmt <109459476+FloSmt@users.noreply.github.com>
2023-09-26 08:26:46 +0200

    small changes

--- src/app/card-input/card-input.component.spec.ts
+++ src/app/card-input/card-input.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardInputComponent } from './card-input.component';
+import {createComponent} from "@angular/core";
 
 describe('CardInputComponent', () => {
   let component: CardInputComponent;
